fb40bf5762bb6af7f50fa2edc48d331a3718293f
PadowYT2 <me@padow.ru>
2025-07-15 15:16:33 +0300

    support for old plasmovoice versions

--- src/main/java/ru/padow/names/PlasmoVoice.java
+++ src/main/java/ru/padow/names/PlasmoVoice.java
@@ -2,13 +2,15 @@ package ru.padow.names;
 
 import org.jetbrains.annotations.NotNull;
 
+import su.plo.slib.api.server.entity.player.McServerPlayer;
 import su.plo.voice.api.addon.AddonInitializer;
 import su.plo.voice.api.addon.InjectPlasmoVoice;
 import su.plo.voice.api.addon.annotation.Addon;
 import su.plo.voice.api.event.EventSubscribe;
 import su.plo.voice.api.server.PlasmoVoiceServer;
-import su.plo.voice.api.server.event.player.PlayerInfoCreateEvent;
-import su.plo.voice.proto.data.player.VoicePlayerInfo;
+import su.plo.voice.api.server.audio.source.ServerAudioSource;
+import su.plo.voice.api.server.audio.source.ServerPlayerSource;
+import su.plo.voice.api.server.event.audio.source.ServerSourceCreatedEvent;
 
 @Addon(
         id = "pv-addon-names",
@@ -25,17 +27,11 @@ public class PlasmoVoice implements AddonInitializer {
     public void onAddonShutdown() {}
 
     @EventSubscribe
-    public void onPlayerInfoCreate(@NotNull PlayerInfoCreateEvent event) {
-        VoicePlayerInfo playerInfo = event.getVoicePlayerInfo();
+    public void onServerSourceCreated(@NotNull ServerSourceCreatedEvent event) {
+        ServerAudioSource<?> source = event.getSource();
+        if (!(source instanceof ServerPlayerSource)) return;
 
-        event.setVoicePlayerInfo(
-                new VoicePlayerInfo(
-                        playerInfo.getPlayerId(),
-                        Names.getConfig()
-                                .players
-                                .getOrDefault(playerInfo.getPlayerId(), playerInfo.getPlayerNick()),
-                        playerInfo.isMuted(),
-                        playerInfo.isVoiceDisabled(),
-                        playerInfo.isMicrophoneMuted()));
+        McServerPlayer player = ((ServerPlayerSource) source).getPlayer().getInstance();
+        source.setName(Names.getConfig().players.getOrDefault(player.getUuid(), player.getName()));
     }
 }
